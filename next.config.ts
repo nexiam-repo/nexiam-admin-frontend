8146ef1e67a4ae3a55dbcee97491d20c990eb644
Gowsik <gowsikreloaded@gmail.com>
2025-06-09 01:01:54 +0200

    updated custom headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Applies to all routes
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload', // 1 year, with subdomains and preload
+          },
+          // Add any other security headers here
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
